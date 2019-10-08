c5144b0bf43e54d58b2ca20c36a5d1b9dd078c3d
Thach An <nhandaotoan1994@gmail.com>
2019-10-08 23:32:14 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route,Switch } from 'react-router-dom';
 import Signup from "./containers/Signup";
-import Login from "./containers/Login";
+import Signin from "./containers/Signin";
+import Home from "./containers/Home";
+import { MDBContainer, MDBListGroup, MDBListGroupItem } from 'mdbreact';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-            <Switch>
-              <Route path="/signup" exact component={Signup} />
-              <Route path="/login" exact component={Login} />
-            </Switch>
-      </HashRouter>
-    );
+      <Router>
+        <MDBContainer>
+          <MDBListGroup style={{ width: "22rem" }}>
+            <MDBListGroupItem><Link to={'/'}> Home </Link></MDBListGroupItem>
+            <MDBListGroupItem><Link to={'/signin'}> Signin </Link></MDBListGroupItem>
+            <MDBListGroupItem><Link to={'/signup'}> Singup </Link></MDBListGroupItem>
+          </MDBListGroup>
+        </MDBContainer>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/signin' component={Signin}/>
+          <Route path='/signup' component={Signup}/>
+        </Switch>
+      </Router>
+
+    )
   }
 }
 
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,37 +1,21 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
-import { Textbox, Radiobox, Checkbox, Select, Textarea } from 'react-inputs-validation';
-import 'react-inputs-validation/lib/react-inputs-validation.min.css';
-import './static/style.css'
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
+
 export default class SignUp extends Component {
   constructor(props){
     super(props)
     this.state={
       username: "",
       password: "",
-      signUpError: ""
+      passwordConfirm: ""
     }
-    this.handleChangeUsername = this.handleChangeUsername.bind(this)
-    this.handleChangePassword = this.handleChangePassword.bind(this)
-    this.handleChangePasswordConfirm = this.handleChangePasswordConfirm.bind(this)
     this.handleSignUp = this.handleSignUp.bind(this)
+    this.changeHandler=this.changeHandler.bind(this)
   }
-  handleChangeUsername(value){
-    this.setState({
-      username: value,
-    })
-  }
-  handleChangePassword(value){
-    this.setState({
-      password: value,
-    })
-  }
-  handleChangePasswordConfirm(value){
-    this.setState({
-      passwordConfirm: value,
-    })
-  }
-  handleSignUp(e) {
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleSignUp() {
     const {
       username,
       password,
@@ -70,22 +54,61 @@ export default class SignUp extends Component {
       passwordConfirm:""
     })
   }
-  render() {
-    return (
-    <form className="form-signin">
-            <h2 className="form-signin-heading"> Please sign in </h2>
-            <label className="sr-only"> Username
-            </label>
-            <input id="username" className="form-control" placeholder="Username" value={this.state.username} onChange={(e)=>this.handleChangeUsername(e.target.value)} required />
-
-            <label className="sr-only"> Password</label>
-            <input type="password" id="password" className="form-control"  value={this.state.password} placeholder="Password" onChange={(e)=>this.handleChangePassword(e.target.value)} required />
-            <label className="sr-only"> Password Comfirm</label>
-            <input type="password" id="password-confirm" className="form-control" value={this.state.passwordConfirm} placeholder="Password Comfirm" onChange={(e)=>this.handleChangePasswordConfirm(e.target.value)} required />
-            <Button variant="outline-primary" onClick={this.handleSignUp}> 
-              Sign in
-            </Button>
-        </form>
+  render(){
+    return(
+    <MDBContainer>
+      <MDBRow>
+      <MDBCol md="6">
+          <form>
+            <p className="h5 text-center mb-4">Sign up</p>
+            <div className="grey-text">
+              <MDBInput
+                label="Your email"
+                icon="envelope"
+                group
+                name="username"
+                type="email"
+                validate
+                error="wrong"
+                success="right"
+                onChange={this.changeHandler}
+                value={this.state.username}
+                className="form-control"
+                required
+                
+              />
+              <MDBInput
+                label="Your password"
+                icon="lock"
+                group
+                type="password"
+                name="password"
+                validate
+                onChange={this.changeHandler}
+                value={this.state.password}
+                className="form-control"
+                required
+              />
+              <MDBInput
+                label="Confirm your password"
+                icon="exclamation-triangle"
+                group
+                type="password"
+                name="passwordConfirm"
+                validate
+                onChange={this.changeHandler}
+                value={this.state.passwordConfirm}
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="text-center">
+              <MDBBtn outline color="default" onClick={this.handleSignUp}>Register</MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
     )
   }
 }
